81c33ce831688a41b6d2441c5c2acc9fc2f7a180
mahdadi <khaled1993mah@gmail.com>
2016-01-05 12:15:32 +0100

    Update mini_project.c

--- mini_project.c
+++ mini_project.c
@@ -1,3 +1,13 @@
 void main() {
+portb = 0b00000001;
+sspstat =0b11000000;
+sspcon =0b00000000  ;
+pir1=0b00000000      ;
+sspcon2=0b00000001    ;
+sspbuf=portb           ;
+sspcon2=0b00000001;
+sspbuf=0b00010000 ;// speed = 10d
+while(sspif!=1);
+
 
 }
