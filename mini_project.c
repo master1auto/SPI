c66d35fb31e2b1a82cbfb53fc2107aee17fd826f
mahdadi <khaled1993mah@gmail.com>
2016-01-07 00:25:26 +0100

    Update mini_project.c

--- mini_project.c
+++ mini_project.c
@@ -1,13 +1,13 @@
-void main() {
-portb = 0b00000001;
-sspstat =0b11000000;
-sspcon =0b00000000  ;
-pir1=0b00000000      ;
-sspcon2=0b00000001    ;
-sspbuf=portb           ;
-sspcon2=0b00000001;
-sspbuf=0b00010000 ;// speed = 10d
-while(sspif!=1);
-
-
+void main() 
+{
+   portbbit.RB0 = 0;             // Activate the SS SPI Select pin
+   sspstat = 0;                  // Set SMP=0 and CKE=1. Notes: The lower 6 bit is read only
+   sspcon1 = 0  ;                // Enable SPI Master with Fosc/4
+   SSPBUF = SPI_SLAVE_ID | ((SPI_SLAVE_speed << 1) & 0x0E)| SPI_SLAVE_WRITE;// Start PIC16F887 OpCode transmission
+while(!SSPSTATbits.BF);          // Wait for Data Transmit/Receipt complete
+   SSPBUF = speed;               // Start PIC16F887 Register Address transmission                               
+while(!SSPSTATbits.BF);          // Wait for Data Transmit/Receipt complete
+   SSPBUF = portbbit.RB0;        // Start Data transmission        
+while(!SSPSTATbits.BF);          // Wait for Data Transmit/Receipt complete
+    portbbit.RB0 = 1;            // CS pin is not active  
 }
