611907da6be10d1bd033ce3afad23103d5527fb2
master1auto_ouargla <microprocessor.m1auto@gmail.com>
2016-01-07 17:41:54 +0100

    Update mini_project.c

--- mini_project.c
+++ mini_project.c
@@ -6,13 +6,13 @@ ANSELH=0;
 TRISB=0;
 TRISC=0;
 
-sspstat = 0;                  // Set SMP=0 and CKE=1. Notes: The lower 6 bit is read only
+sspstat = 0;                  // Set SMP=0 and CKE=0. Notes: The lower 6 bit is read only
 sspcon = 0x20;                // Enable SPI Master with Fosc/4
 while(1){
-portb.f0 = 0;             // Activate the SS SPI Select pin
+portb.f0 = 0;             
 SSPBUF = speed;               // Start PIC16F887 Register Address transmission
-while(!SSPIF_bit);          // Wait for Data Transmit/Receipt complete
-portb.f0 = 1;            // CS pin is not active
+while(!SSPIF_bit);          // Wait for Data Transmit to complete
+portb.f0 = 1;            
 SSPIF_bit=0;
 delay_ms(100);
 }
