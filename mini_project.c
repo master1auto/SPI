41e1cd4e69959666f886517d70a7293eb05e78f2
master1auto_ouargla <microprocessor.m1auto@gmail.com>
2016-01-07 22:19:07 +0100

    Update mini_project.c

--- mini_project.c
+++ mini_project.c
@@ -1,4 +1,4 @@
-void spi_init(){
+void myspi_init(){
 sspstat = 0;                  // Set SMP=0 and CKE=0. Notes: The lower 6 bit is read only
 sspcon = 0x20;                // Enable SPI Master with Fosc/4  
 }
@@ -9,7 +9,7 @@ ANSEL=0;
 ANSELH=0;
 TRISB=0;
 TRISC=0;
-spi_init();
+myspi_init();
 while(1){
 portb.f0 = 0;             
 SSPBUF = speed;               // Start PIC16F887 Register Address transmission
